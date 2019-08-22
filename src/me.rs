3c01961912de1e173bfc3241bee55ed4d58700f8
Kyle Siefring <kylesiefring@gmail.com>
2019-08-22 12:50:02 -0700

    0.6

--- src/me.rs
+++ src/me.rs
@@ -205,7 +205,7 @@ pub trait MotionEstimation {
           get_mv_range(fi.w_in_b, fi.h_in_b, frame_bo, blk_w, blk_h);
 
         // 0.5 is a fudge factor
-        let lambda = (fi.me_lambda * 256.0 * 0.4) as u32;
+        let lambda = (fi.me_lambda * 256.0 * 0.6) as u32;
 
         // Full-pixel motion estimation
 
