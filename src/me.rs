50c0b5dad62b1975e384b26beb0badf1bc994376
Kyle Siefring <kylesiefring@gmail.com>
2020-07-18 13:57:11 -0400

    Remove from full res me

--- src/me.rs
+++ src/me.rs
@@ -357,7 +357,7 @@ pub trait MotionEstimation {
       let mut best_mv = MotionVector::default();
 
       // 0.5 is a fudge factor
-      let lambda = (fi.me_lambda * 256.0 * 0.5) as u32;
+      let lambda = 0;
 
       Self::full_pixel_me(
         fi,
